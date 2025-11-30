1fcd526c565cc4fb60a9d1381a458aa49c622fc8
Ario <Ariobashiri@gmail.com>
2025-11-30 18:56:41 +0300

    small file adjustments

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 
 public class Main {
 
-    ArrayList<Student>  students = new ArrayList<>();
-    ArrayList<Course>  courses = new ArrayList<>();
-    ArrayList<Classroom>  classrooms = new ArrayList<>();
+    ArrayList<Student> students = new ArrayList<>();
+    ArrayList<Course> courses = new ArrayList<>();
+    ArrayList<Classroom> classrooms = new ArrayList<>();
 
 
     public static void Calculate(ArrayList<Classroom>  classrooms,ArrayList<Course>  courses,ArrayList<Student>  students){
