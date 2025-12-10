685b41a112199d40d953fe90f54a0f744ed07b2e
simgeg143 <simgegokalp1@hotmail.com>
2025-12-11 00:37:37 +0300

    Add block calculation and scheduling validation methods to Main
    Added block calculation methods to handle 30-minute allocations and availability checks.

--- Main.java
+++ Main.java
@@ -7,9 +7,29 @@ public class Main {
     ArrayList<Classroom> classrooms = new ArrayList<>();
 
 
+    // block calculations
+    public static void calculateBlock(int startBlock, int durationMinutes, Classroom room) {
+        int block = (int) Math.ceil(durationMinutes / 30.0); // how many blocks needed
+
+        for (int i = 0; i <= block; i++) {
+            room.getHours()[startBlock+i] = 1; // block is marked as occupied
+        }
+    }
+    // control block is marked as occupied or not
+    public static boolean canCalculateBlock(int startBlock, int durationMinutes, Classroom room) {
+        int block = (int) Math.ceil(durationMinutes / 30.0);
+        for (int i = 0; i <= block; i++) {
+            if (room.getHours()[startBlock+i] == 1){
+                return false; // can not schedule
+            }
+        }
+        return true; // all blocks empty
+    }
     public static void calculate(ArrayList<Classroom>  classrooms,ArrayList<Course>  courses,ArrayList<Student>  students){
 
+
     }
+
 
 
 }
