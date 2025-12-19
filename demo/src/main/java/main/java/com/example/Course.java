c71eef8a90b92501de023b9348dd603ebc14ef52
Ario <Ariobashiri@gmail.com>
2025-12-19 03:24:55 +0300

    Added exam time related variables and methods
    Courses can now hold the starting and the ending hour of exam (as both a String and integer) and can calculate all of them using only the begging time of exam.

--- demo/src/main/java/main/java/com/example/Course.java
+++ demo/src/main/java/main/java/com/example/Course.java
@@ -7,7 +7,12 @@ public class Course {
     public boolean alreadyScheduled = false;
     Student[] attendees;
     ArrayList<Classroom> examClass;
-    int examDuration;
+    int examDuration; // in minutes
+    String timeOfExam; // The scheduled time (add 9*60 minutes to find the exam hour in minutes, then divide it by 60 to find hour and minute.)
+    int examHour, examMinute; // in case the exam hour and date are needed as integers.
+
+    String endOfExam; // the time exam finished
+    int endOfExamHour, endOfExamMinute; // the hour and minute exam finishes.
 
     public Course(int code, Student[] attendees, ArrayList<Classroom> examClass, int examDuration) {
         this.code = code;
@@ -36,10 +41,6 @@ public class Course {
         return examClass;
     }
 
-    public void setExamClass(ArrayList<Classroom> examClass) {
-        this.examClass = examClass;
-        alreadyScheduled = true;
-    }
 
     public int getExamDuration() {
         return examDuration;
@@ -47,5 +48,46 @@ public class Course {
 
     public void setExamDuration(int examDuration) {
         this.examDuration = examDuration;
+    }
+
+    public void setExamClass(ArrayList<Classroom> examClass) {
+        this.examClass = examClass; // the class in which the exam will be held
+        alreadyScheduled = true; // the exam is scheduled 
+    }
+
+    public void setTimeOfExam(int startBlock){
+        /*
+            This method calculates the hour and the minute the exam starts.
+        */
+        int time = startBlock * Main.eachBlockDuration;
+        time += 9 * 60;
+        this.examHour = time / 60;
+        this.examMinute = time % 60;
+        this.timeOfExam = Integer.toString(this.examHour) + ":" + Integer.toString(this.examMinute);
+        calculateEndOfExam();
+    }
+
+    private void calculateEndOfExam(){
+        /*
+            This method calculates the hour and the minute the exam finishes.
+        */
+        int total = this.examHour * 60 + this.examMinute + examDuration;
+        this.endOfExamHour = total / 60;
+        this.endOfExamMinute = total % 60;
+        endOfExam = Integer.toString(this.endOfExamHour) + ":" + Integer.toString(this.endOfExamMinute);
+    }
+
+    public String getEndOfExam(){
+        return endOfExam;
+    }
+
+    public int getExamHour(){
+        return this.examHour;
+    }
+    public int getExamMinute(){
+        return this.examMinute;
+    }
+    public String getTimeOfExam(){
+        return this.timeOfExam;
     }
 }
